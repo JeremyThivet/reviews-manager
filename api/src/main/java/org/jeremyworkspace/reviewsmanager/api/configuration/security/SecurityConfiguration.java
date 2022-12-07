bcb25085fb9bc1012a07b5bd20c1bd10433aec90
jeremy <thivetj@gmail.com>
2022-12-07 18:22:00 +0100

    Backend : Modifying frontend url to enable smooth browsing for user Frontend : N/A

--- api/src/main/java/org/jeremyworkspace/reviewsmanager/api/configuration/security/SecurityConfiguration.java
+++ api/src/main/java/org/jeremyworkspace/reviewsmanager/api/configuration/security/SecurityConfiguration.java
@@ -46,7 +46,30 @@ public class SecurityConfiguration {
         authenticationManagerBuilder.authenticationProvider(provider);
         AuthenticationManager authenticationManager = authenticationManagerBuilder.getOrBuild();
 
+        // Preparing URLS
         String loginUrl = "/api/login";
+        final String[] FRONT_END_URLS = {
+                "/",
+                "/inscription",
+                "/connexion",
+                "/mentionslegales",
+                "/mesclassements",
+                "/gestionclassements",
+                "/editerclassement/*",
+                "/consulterclassement/*",
+                "/deconnexion",
+                "/assets/**",
+                "/static/**",
+                "/version"
+        };
+        final String[] API_PUBLIC_GET_ENDPOINTS = {
+                "/api/refreshToken",
+                "/api/users/count",
+                "/api/logout"
+        };
+        final String[] API_PUBLIC_POST_ENDPOINTS = {
+                "/api/users"
+        };
 
         http
                 //.requiresChannel(channel -> channel.anyRequest().requiresSecure())
@@ -56,25 +79,20 @@ public class SecurityConfiguration {
                 /*.authorizeRequests()
                 .antMatchers("/**").permitAll();*/
                 .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager, jwtService, loginUrl))
                 .addFilterAfter(new JwtTokenVerifierFilter(jwtService, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
 
                 // Authorizations
                 .authorizeRequests()
-                    // Public URL
-                    .antMatchers(HttpMethod.POST, loginUrl).permitAll()
-                    .antMatchers(HttpMethod.GET, "/").permitAll()
-                    .antMatchers(HttpMethod.GET, "/version").permitAll()
-                    .antMatchers(HttpMethod.GET, "/assets/**").permitAll()
-                    .antMatchers(HttpMethod.GET, "/static/**").permitAll()
-                    .antMatchers(HttpMethod.GET, "/api/refreshToken").permitAll()
-                    .antMatchers(HttpMethod.POST, "/api/users").permitAll()
-                    .antMatchers(HttpMethod.GET, "/api/users/count").permitAll()
-                    .antMatchers(HttpMethod.GET, "/api/logout").permitAll()
-                    // Authenticated URL
-                    .anyRequest().authenticated()
+                // Public URL
+                .antMatchers(HttpMethod.POST, loginUrl).permitAll()
+                .antMatchers(HttpMethod.GET, FRONT_END_URLS).permitAll()
+                .antMatchers(HttpMethod.GET, API_PUBLIC_GET_ENDPOINTS).permitAll()
+                .antMatchers(HttpMethod.POST, API_PUBLIC_POST_ENDPOINTS).permitAll()
+                // Others URLs require authentication by default
+                .anyRequest().authenticated()
                 .and()
                 .authenticationManager(authenticationManager);
 
--- api/src/main/java/org/jeremyworkspace/reviewsmanager/api/controller/IndexController.java
+++ api/src/main/java/org/jeremyworkspace/reviewsmanager/api/controller/IndexController.java
@@ -7,14 +7,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
     @Value("${app.version}")
     private String appVersion;
 
-    @GetMapping("/")
-    public ModelAndView index() {
+    // List of front end urls available for which we need to serve the content.
+    @GetMapping({
+            "/",
+            "/inscription",
+            "/connexion",
+            "/mentionslegales",
+            "/mesclassements",
+            "/gestionclassements",
+            "/editerclassement/*",
+            "/consulterclassement/*",
+            "/deconnexion"
+    })
+    public ModelAndView index(HttpServletRequest request) {
         ModelAndView mav=new ModelAndView("index");
         return mav;
     }
